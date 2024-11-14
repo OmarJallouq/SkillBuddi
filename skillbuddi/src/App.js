5b17ea55bcfa87f4b372b6faf21634565c388fd6
Omar Jallouq <omarjallouq@gmail.com>
2024-11-14 18:37:06 +0100

    Fixed redundant Home import

--- skillbuddi/src/App.js
+++ skillbuddi/src/App.js
@@ -6,7 +6,6 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Register from './pages/Register';
-import Home from './pages/Home';
 
 const App = () => {
   return (
