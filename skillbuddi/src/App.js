4a04703be7a374ea0cea0d742647a4191197a41c
Omar Jallouq <omarjallouq@gmail.com>
2024-11-14 19:51:54 +0100

    Added Register Function

--- skillbuddi/src/App.js
+++ skillbuddi/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/login" element={<Login />} />
-
+          <Route path="/register" element={<Register />} />
           <Route element={<PrivateRoutes />}>
             {/* PUT THE ROUTES HERE THAT SHOULD ONLY BE ACCESSED IF THE USER IS LOGGED IN */}
             <Route path="/" element={<Home />} />
