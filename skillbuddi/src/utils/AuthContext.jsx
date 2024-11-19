75d0fe451a197a270ea0a82827d7d7039a125715
Omar Jallouq <omarjallouq@gmail.com>
2024-11-19 17:08:34 +0100

    Fixed conflicts in AuthContext.jsx

--- skillbuddi/src/utils/AuthContext.jsx
+++ skillbuddi/src/utils/AuthContext.jsx
@@ -1,99 +1,3 @@
-/*
-
-export const AuthProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    checkUserStatus();
-  }, []);
-
-  const loginUser = async (userInfo) => {
-    setLoading(true);
-    try {
-      await account.createEmailPasswordSession(
-        userInfo.email,
-        userInfo.password
-      );
-      let accountDetails = await account.get();
-      setUser(accountDetails);
-
-      return { success: true };
-    } catch (error) {
-      setError(error.message || "Something went wrong");
-      return { success: false, error: error.message || "Something went wrong" };
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const logoutUser = async () => {
-    try {
-      await account.deleteSessions("current");
-      return { success: true };
-    } catch (error) {
-      setError(error.message || "Something went wrong");
-      return { success: false, error: error.message || "Something went wrong" };
-    } finally {
-      setUser(null);
-    }
-  };
-
-  const registerUser = async (userInfo) => {
-    setLoading(true);
-    try {
-      await account.create(
-        ID.unique(),
-        userInfo.email,
-        userInfo.password,
-        userInfo.firstName + " " + userInfo.lastName
-      );
-
-      await account.createEmailPasswordSession(
-        userInfo.email,
-        userInfo.password
-      );
-      let accountDetails = await account.get();
-      setUser(accountDetails);
-      return { success: true };
-    } catch (error) {
-      setError(error.message || "Something went wrong");
-      return { success: false, error: error.message || "Something went wrong" };
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const checkUserStatus = async () => {
-    try {
-      let accountDetails = await account.get();
-      setUser(accountDetails);
-    } catch (error) {}
-    setLoading(false);
-  };
-
-  const contextData = {
-    user,
-    loginUser,
-    logoutUser,
-    registerUser,
-  };
-
-  return (
-    <>
-      <AuthContext.Provider value={contextData}>
-        {loading ? <p>Loading...</p> : children}
-      </AuthContext.Provider>
-    </>
-  );
-};
-
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
-export default AuthContext;*/
-
 import { useContext, useState, useEffect, createContext } from "react";
 import { account, databases, storage } from "../appwriteConfig";
 import { ID } from "appwrite";
@@ -202,7 +106,8 @@ export const AuthProvider = ({ children }) => {
       );
       return { success: true };
     } catch (error) {
-      return { success: false, error: error.message };
+      setError(error.message || "Something went wrong");
+      return { success: false, error: error.message || "Something went wrong" };
     }
   };
 
@@ -220,9 +125,7 @@ export const AuthProvider = ({ children }) => {
     try {
       const accountDetails = await account.get();
       setUser(accountDetails);
-    } catch (error) {
-      console.error("No user logged in");
-    }
+    } catch (error) {}
     setLoading(false);
   };
 
@@ -234,6 +137,7 @@ export const AuthProvider = ({ children }) => {
     updateProfile,
     uploadProfilePicture,
   };
+  //TODO: check this
 
   return (
     <AuthContext.Provider value={contextData}>
