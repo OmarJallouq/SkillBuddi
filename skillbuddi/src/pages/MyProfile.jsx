23210f266637503b1fb0ced08e9f712c493ac258
Omar Jallouq <omarjallouq@gmail.com>
2024-11-25 17:41:11 +0100

    Implemented Skill adding and removing with DB

--- skillbuddi/src/pages/MyProfile.jsx
+++ skillbuddi/src/pages/MyProfile.jsx
@@ -1,17 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SkillTag from "../components/SkillTag";
 import { useAuth } from "../utils/AuthContext";
+import { useDatabase } from "../utils/DatabaseContext";
+import { toast } from "react-toastify";
 import "../styles/myProfile.css";
 
 const MyProfile = () => {
   const { user } = useAuth();
+  const { updateUserData } = useDatabase();
+  const [skills, setSkills] = useState([]);
 
-  const handleRemoveSkill = (skillToRemove) => {
-    //TODO: HandleRemoveSkill Logic with DB
+  useEffect(() => {
+    setSkills(user.Skills);
+    console.log(skills);
+  }, []);
+
+  const handleRemoveSkill = async (skillToRemove) => {
+    const updatedSkills = skills.filter((skill) => skill !== skillToRemove);
+
+    try {
+      const response = await updateUserData(user.$id, {
+        Skills: updatedSkills,
+      });
+
+      if (response.success) {
+        setSkills(updatedSkills);
+        toast.success("Skill Removed Successfully!");
+      } else {
+        throw new Error(response.error);
+      }
+    } catch (error) {
+      toast.error(error.message || "Failed to remove skill.");
+    }
   };
 
-  const handleAddSkill = (newSkill) => {
-    //TODO: HandleAddSkill Logic with DB
+  const handleAddSkill = async (newSkill) => {
+    if (!newSkill.trim()) {
+      toast.error("Skill cannot be empty!");
+      return;
+    }
+
+    const currentSkills = skills || [];
+
+    if (currentSkills.includes(newSkill)) {
+      toast.error("Skill already exists!");
+      return;
+    }
+
+    const updatedSkills = [...currentSkills, newSkill];
+
+    try {
+      const response = await updateUserData(user.$id, {
+        Skills: updatedSkills,
+      });
+      if (response.success) {
+        setSkills(updatedSkills); // Update local state immediately for UI feedback
+        toast.success("Skill Added Successfully!");
+      } else {
+        throw new Error(response.error);
+      }
+    } catch (error) {
+      toast.error(error.message || "Failed to add skill.");
+    }
   };
 
   return (
@@ -29,11 +79,11 @@ const MyProfile = () => {
         <div className="skills-section">
           <div className="title-skills-container">
             <h2>My Skills</h2>
-            {!user.skills ? (
+            {skills.length === 0 ? (
               <p>No skills added yet.</p>
             ) : (
               <ul className="skills-list">
-                {user.skills.map((skill, index) => (
+                {skills.map((skill, index) => (
                   <SkillTag
                     skill={skill}
                     index={index}
