db79787956eae2a0a9dabe3f6ba213d85d4c7591
FilipJ <filip.janczak@studbocconi.it>
2024-11-27 20:32:50 +0100

    set up icons for about page, work tbc

--- skillbuddi/src/pages/About.jsx
+++ skillbuddi/src/pages/About.jsx
@@ -11,29 +11,34 @@ const About = () => {
         </p>
       </div>
       <div className="content-section">
-        <h2 className="section-title">Our Mission</h2>
-        <p className="text">
-          Our platform is designed to bring individuals together to exchange
-          skills, learn from one another, and grow as a community. Whether you
-          want to teach coding, learn a new language, or improve your painting
-          skills, we make it easy to connect with others who share your
-          passions.
-        </p>
+        <div className="logo-section">
 
-        <h2 className="section-title">Why Choose Us?</h2>
-        <p className="text">
-          - Personalized profiles to showcase your skills and interests <br />
-          - Matching with like-minded individuals <br />
-          - A safe and friendly environment for collaboration
-        </p>
+        </div>
+        <div className="text-section">
+          <h2 className="section-title">Our Mission</h2>
+          <p className="text">
+            Our platform is designed to bring individuals together to exchange
+            skills, learn from one another, and grow as a community. Whether you
+            want to teach coding, learn a new language, or improve your painting
+            skills, we make it easy to connect with others who share your
+            passions.
+          </p>
 
-        <h2 className="section-title">Our Values</h2>
-        <p className="text">
-          We believe in the power of collaboration, lifelong learning, and
-          fostering meaningful connections. Our goal is to empower individuals
-          to achieve their full potential by sharing what they know and learning
-          from others.
-        </p>
+          <h2 className="section-title">Why Choose Us?</h2>
+          <p className="text">
+            - Personalized profiles to showcase your skills and interests <br />
+            - Matching with like-minded individuals <br />
+            - A safe and friendly environment for collaboration
+          </p>
+
+          <h2 className="section-title">Our Values</h2>
+          <p className="text">
+            We believe in the power of collaboration, lifelong learning, and
+            fostering meaningful connections. Our goal is to empower individuals
+            to achieve their full potential by sharing what they know and learning
+            from others.
+          </p>
+        </div>
       </div>
     </div>
   );
